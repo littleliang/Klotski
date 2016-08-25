dc85c42dd1fce403240f4044f901f486ed4ff864
littleliang <841412947@qq.com>
2016-08-25 18:54:35 +0800

    correct clonum

--- src/klotski/util/ConstValue.java
+++ src/klotski/util/ConstValue.java
@@ -2,6 +2,6 @@ package klotski.util;
 
 public class ConstValue {
 	public static final int rowNum = 5;
-	public static final int colNum = 5;
+	public static final int colNum = 4;
 	public static final int nodeNum = 10;
 }
